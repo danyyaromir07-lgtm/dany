96345020de29395d7910692719553587152b2ec3
danyyaromir07-lgtm <dany.yaromir07@gmail.com>
2026-08-14 15:29:46 +0200

    Switch long drawing-code OCR to cropped v5

--- vector-titleblock-ocr-v1.js
+++ vector-titleblock-ocr-v1.js
@@ -1,3 +1,3 @@
 // Compatibility shim: title-block OCR v2.
 import './vector-titleblock-ocr-v2.js?v=20260813-partialtoken1';
-import './vector-titleblock-long-code-v4.js?v=20260814-longcode5';
+import './vector-titleblock-long-code-v5.js?v=20260814-longcode6';
