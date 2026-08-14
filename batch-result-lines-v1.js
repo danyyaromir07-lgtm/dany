53474ba3aec0231342d4e2121200c14f8b828349
danyyaromir07-lgtm <dany.yaromir07@gmail.com>
2026-08-14 10:49:40 +0200

    Load post-analysis signature detection synchronizer

--- batch-result-lines-v1.js
+++ batch-result-lines-v1.js
@@ -1,4 +1,5 @@
 import './batch-search-variants-v1.js?v=20260814-searchvariants1';
+import './signature-analysis-sync-v1.js?v=20260814-signaturesync1';
 
 const TABLE = '#batchTable';
 const STYLE_ID = 'batch-result-lines-style';
