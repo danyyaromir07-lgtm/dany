3139cac37be81ff85710edc11c0b041859ab3442
danyyaromir07-lgtm <dany.yaromir07@gmail.com>
2026-08-17 12:48:06 +0200

    Load vertical OCR supplement for full drawing codes

--- vector-titleblock-ocr-v1.js
+++ vector-titleblock-ocr-v1.js
@@ -1,6 +1,7 @@
 // Compatibility shim: title-block OCR v2.
 import './vector-titleblock-ocr-v2.js?v=20260817-rotationfix1';
 import './vector-titleblock-long-code-v5.js?v=20260814-longcode6';
+import './vector-titleblock-long-code-vertical-v1.js?v=20260817-longvertical1';
 import './vector-titleblock-safe-code-v1.js?v=20260817-safecode2';
 import './vector-titleblock-safe-code-adaptive-v1.js?v=20260817-dedup2';
 import './vector-titleblock-safe-code-vertical-v1.js?v=20260817-vertical1';
