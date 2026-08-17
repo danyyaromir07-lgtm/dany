6568aaabdb92b923c76e9beac086071c5d132ad4
danyyaromir07-lgtm <dany.yaromir07@gmail.com>
2026-08-17 12:49:55 +0200

    Refresh titleblock loader for vertical full-code apply

--- vector-titleblock-ocr-v1.js
+++ vector-titleblock-ocr-v1.js
@@ -1,5 +1,5 @@
 // Compatibility shim: title-block OCR v2.
-import './vector-titleblock-ocr-v2.js?v=20260817-rotationfix1';
+import './vector-titleblock-ocr-v2.js?v=20260817-longvertical1';
 import './vector-titleblock-long-code-v5.js?v=20260814-longcode6';
 import './vector-titleblock-long-code-vertical-v1.js?v=20260817-longvertical1';
 import './vector-titleblock-safe-code-v1.js?v=20260817-safecode2';
